3e133e3202177f1c3f097e48f5192de86f0ee0e2
DESKTOP-58C525U\Maria <irimescu.mariaalexandra@yahoo.com>
2018-08-27 14:29:09 +0300

    End of Section 6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,26 +10,25 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private alertify: AlertifyService) {}
 
   ngOnInit() {}
 
   // Metoda pentru login
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in successfully');
+      this.alertify.success('Logged in successful');
     }, error => {
-      console.log('Failed to login');
+     this.alertify.error('Failed to login');
     });
   }
 
  loggedIn() {
-   const token = localStorage.getItem('token');
-   return !!token;
+   return this.authService.loggedIn();
  }
 
  logout() {
    localStorage.removeItem('token');
-   console.log('Loggoed out');
+ this.alertify.message('Loggoed out');
  }
 }
